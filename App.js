91c41e2c0b989b26bddb66c03db3e8019e46ea01
Arjun Thapa Magar <arjun2057@hotmail.com>
2023-09-03 16:34:50 +1000

    complete Todo App (Part 2)

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 
 import react, { useState } from 'react';
 import { FlatList, StyleSheet, Text, View, } from 'react-native';
-import Header from './components/header'
+import Header from './components/header';
+import TodoItem from './components/todoItem';
 
 export default function App() {
 
@@ -10,6 +11,12 @@ export default function App() {
     { text: 'create an app', key: '2' },
     { text: 'play on the switch', key: '3' }
   ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
 
   return (
     <View style={styles.container}>
@@ -23,7 +30,7 @@ export default function App() {
           <FlatList 
             data = {todos}
             renderItem={({ item }) => (
-              <Text>{item.text}</Text>
+              <TodoItem item = {item} pressHandler={pressHandler} />
             )}
           />
 
